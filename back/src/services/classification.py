688c6fee9a7b769c3e5b20e4741548f26a8a8c45
Ana Sofía Padilla Daza <78044651+aspd-2501@users.noreply.github.com>
2024-04-20 16:50:34 -0500

    debería estar así

--- back/src/services/classification.py
+++ back/src/services/classification.py
@@ -3,8 +3,6 @@ import numpy as np
 import nltk
 import spacy
 import string
-import seaborn as sns
-import matplotlib.pyplot as plt
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('words')
@@ -30,8 +28,6 @@ from sklearn.model_selection import KFold
 
 from sklearn.ensemble import RandomForestClassifier
 
-from matplotlib import style
-from sklearn.linear_model import LogisticRegression
 import sklearn
 from sklearn.svm import SVC
 from sklearn import metrics
@@ -44,7 +40,6 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 from sklearn.pipeline import Pipeline
 from joblib import dump, load
-import io
 
 def definirStopwords():
     palabrasEliminar = set(stopwords.words('spanish'))
@@ -83,7 +78,7 @@ def removerTodos(texto):
 
 def vectorizacion(texto):
     df = pd.read_csv('BagOfWords.csv', sep=',', encoding = 'utf-8')
-    vectorizado = texto.map(str)
+    vectorizado = texto["Review"].map(str)
     count = CountVectorizer()
     bag_of_words = count.fit_transform(vectorizado)
     bag_of_words_array = bag_of_words.toarray()
@@ -103,39 +98,24 @@ def vectorizacion(texto):
         if col not in df.columns:
             result_df[col] = 0
 
-    result_df = result_df.fillna(int(0))
+    result_df_new = result_df.fillna(int(0))
+    return result_df_new
 
 
-def classify_multiple_texts(texto) -> pd.DataFrame:
-    if not isinstance(texto, pd.DataFrame):
-        texts_df = pd.DataFrame([texto], columns=['Review'])
+def classify_single_text(texto):
+    if isinstance(texto, pd.DataFrame) != True:
+        texto = pd.DataFrame([texto], columns=['Review'])
 
-    df_columns = texts_df.columns
-
-    if len(df_columns) > 1:
-        raise Exception("The dataframe must have only one column")
-    elif len(df_columns) < 1:
-        raise Exception("The dataframe must have at least one column")
+    preprocessed_text = removerTodos(texto)
+    result_df = vectorizacion(preprocessed_text)
     
-    column_name = df_columns[0]
-    for i in range(len(texts_df[column_name])):
-        limpio = removerTodos(texts_df[column_name].iloc[i])
-        texts_df[column_name].iloc[i] = limpio
-
-    vectorizacion(text_df)
-
-    new_X = texts_df[column_name]
     pipeline = load('model.joblib')
-    class_column = pipeline.predict(new_X)
-    texts_df["Class"] = class_column
-    texts_df["Review"] = new_X
-    return texts_df
-
-def classify_single_text(text: str) -> int:
-    if not isinstance(texto, pd.DataFrame):
-        texts_df = pd.DataFrame([texto], columns=['Review'])
-        
-    pipeline = load('model.joblib')
-    preprocessed_text = removerTodos(text)
     
-    return pipeline.predict([preprocessed_text])[0], preprocessed_text
+    Y = result_df['Class']
+    X = result_df.drop(['Class'], axis=1)
+
+    y_pred = pipeline.predict(X)
+
+    result_df['Class'] = y_pred
+    
+    return y_pred
